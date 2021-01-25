0b19ed10a2e2c62e4e377a4849a06ac71bc26f97
Lennart Weihs <lennart@hubblr.io>
2021-01-25 17:47:07 +0100

    Updates

---
+++ precache-manifest.8bcdf0fd3eca53d06859f81f4b352a58.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa6f40b5431d0c47ff0d3bf44708a99",
+    "url": "./index.html"
+  },
+  {
+    "revision": "592769180473784eba59",
+    "url": "./static/css/main.6d0415d6.chunk.css"
+  },
+  {
+    "revision": "b4a4d03795629f2d75b6",
+    "url": "./static/js/2.b72b85b2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b72b85b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "592769180473784eba59",
+    "url": "./static/js/main.8f99f84d.chunk.js"
+  },
+  {
+    "revision": "72d19479477b6bd19354",
+    "url": "./static/js/runtime-main.6cc29995.js"
+  },
+  {
+    "revision": "99ca1299e55af8fb9c029e4a4d6d228e",
+    "url": "./static/media/cat-2934720_640.99ca1299.jpg"
+  },
+  {
+    "revision": "efc4ec2ded9c0b5b334a813c04093ea3",
+    "url": "./static/media/cat-694730_640.efc4ec2d.jpg"
+  },
+  {
+    "revision": "461b4ded8294c3ada49f55cb9ad974c8",
+    "url": "./static/media/kitty-2948404_640.461b4ded.jpg"
+  }
+]);
